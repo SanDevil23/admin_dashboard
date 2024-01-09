f358bd63e98e8f7d0f68f8bb25fc860aad0ffe70
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-01-09 22:58:23 +0530

    logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import Logo from './Logo';
 function Header() {
   return (
     <header id='header' className='header fixed-top d-flex align-items-center'>
-        Header
         {/*logo */}
         <Logo/>
         {/*search bar */}
