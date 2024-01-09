ec1314a9b79a673cfbe1d3fa449def0309c1cbcc
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-01-09 22:44:18 +0530

    adding logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import './logo.css'
 
 function Logo() {
+  const handleToggleSideBar = () => {
+    document.body.classList.toggle('toggle-sidebar');
+  };
   return (
-    <div>
-        Logo
+    <div className='d-flex align-items-center justify-content-between'>
+      <a href='/' className='logo d-flex align-items-center'>
+        {}
+        <span className='d-none d-lg-block'>AdminDashboard</span>
+      </a>
+      <i className='bi bi-list toggle-sidebar-btn' onClick={handleToggleSideBar}></i>
     </div>
   )
 }
