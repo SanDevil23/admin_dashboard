f901eac4edbbeb1c69db2f14992f6387b189c22d
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-01-15 22:53:08 +0530

    editing nav

--- src/components/NavMessage.jsx
+++ src/components/NavMessage.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 
 function NavMessage() {
   return (
-    <div>
-      NavMessage
-    </div>
+    <li className='nav-item dropdown'>
+      <a className="nav-link nav-icon" href="/" data-bs-toggle="dropdown">
+        <i className='bi bi-chat-left-text'></i>
+        <span className='badge bg-success badge-number'>3</span>
+      </a>
+
+      <ul className='dropdown-menu dropdown-menu-end dropdown-menu-arrow messages'>
+        <li className='dropdown-header'>
+          You have 3 new messages
+          <a href="/">
+            <span className='badge rounded-pill bg-primary p-2 ms-2'>
+              View All
+            </span>
+          </a>
+        </li>
+
+        <li>
+          <hr className='dropdown-divider'/>
+        </li>
+      </ul>
+    </li>
   )
 }
 
